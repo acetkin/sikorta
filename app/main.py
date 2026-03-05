903d0580baf5cbcde70f0874f04a6c2579279d35
acetkin <acetkin06@gmail.com>
2026-03-05 16:07:17 +0800

    SIK-0003 add /hello endpoint

--- app/main.py
+++ app/main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get("/health", response_class=PlainTextResponse)
 def health() -> str:
     return "ok"
+
+
+@app.get("/hello", response_class=PlainTextResponse)
+def hello() -> str:
+    return "hello"
